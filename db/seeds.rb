c82cad69ae5b34d9133e422f4d10bdddbcbcaa1c
Nicolas Debray <nicdebray@gmail.com>
2018-10-31 15:49:45 +0100

    create songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  cat = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  resto = Restaurant.create!(name: Faker::Artist.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: cat)
+  puts "#{resto.name} created"
+end
